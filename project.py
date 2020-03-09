b157f65fb10da53311f32bf40b5cfde88e321e5d
davcaste <55695300+davcaste@users.noreply.github.com>
2020-03-09 12:45:25 +0100

    individuation of correspondences
    individuation of correspondences in the two videos choosing the most similar keypoints

--- project.py
+++ project.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import matplotlib.patches as patches
+import matplotlib.pyplot as plt
 
 
 capL = cv2.VideoCapture('robotL.avi')
@@ -27,16 +28,14 @@ while capL.isOpened() or capR.isOpened():
         centerL = frame[int(frame.shape[0]/2)-dim: int(frame.shape[0]/2)+dim, int(frame.shape[1]/2)-dim:int(frame.shape[1]/2)+dim]
         centerR = frame1[int(frame1.shape[0] / 2) - dim: int(frame1.shape[0] / 2) + dim,
                   int(frame1.shape[1] / 2) - dim:int(frame1.shape[1] / 2) + dim]
-        cv2.imshow('centerL',centerL)
-        cv2.imshow('centerR', centerR)
 
 
         gray = cv2.cvtColor(centerL, cv2.COLOR_BGR2GRAY)
         sift = cv2.xfeatures2d.SIFT_create()
-        kp, des = sift.detectAndCompute(gray, None)
+        kp2, des2 = sift.detectAndCompute(gray, None)
     # draw key points detected
-        img = cv2.drawKeypoints(gray, kp, gray, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        cv2.imshow("grayframe", img)
+        img2 = cv2.drawKeypoints(gray, kp2, gray, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        cv2.imshow("grayframe", img2)
 
         gray1 = cv2.cvtColor(centerR, cv2.COLOR_BGR2GRAY)
         sift = cv2.xfeatures2d.SIFT_create()
@@ -45,15 +44,73 @@ while capL.isOpened() or capR.isOpened():
         img1 = cv2.drawKeypoints(gray1, kp1, gray1, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         cv2.imshow("grayframe1", img1)
 
+        distance = np.array(np.sqrt(np.sum((des1[:, np.newaxis, :] - des2[np.newaxis, :, :]) ** 2, axis=-1))) #SIFT descriptor of a point is just 128-dimensional vector, so you can simple compute Euclidean distance between every two and match nearest pairs.
+        ind = np.unravel_index(np.argmin(distance, axis=None), distance.shape)
+        print(distance.min())
+        kpL = kp2[ind[1]]
+        kpR = kp1[ind[0]]
+        cv2.circle(centerL, (int(kpL.pt[0]), int(kpL.pt[1])), 4, (255, 0, 0), 1)
+        cv2.imshow('centerL', centerL)
+        cv2.circle(centerR, (int(kpR.pt[0]), int(kpR.pt[1])), 4, (255, 0, 0), 1)
+        cv2.imshow('centerR', centerR)
+        # # create BFMatcher object
+        # bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        #
+        # # Match descriptors.
+        # matches = bf.match(des1, des2)
+        #
+        # # Sort them in the order of their distance.
+        # matches = sorted(matches, key=lambda x: x.distance)
+        #
+        # # Draw first 10 matches.
+        # img3 = cv2.drawMatches(img1, kp1, img2, kp2, matches[:10], flags=2)
+        #
+        # plt.imshow(img3), plt.show()
 
         cv2.imshow('FrameL', frame)
         cv2.imshow('FrameR', frame1)
     # Press Q on keyboard to  exit
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
-print('ciao')
 
 capL.release()
 capR.release()
 
 cv2.destroyAllWindows()
+# print(distance)
+print('ciao')
+print(distance.min())
+ind = np.unravel_index(np.argmin(distance, axis=None), distance.shape)
+# print(ind)
+# print(distance[ind])
+kpL = kp2[ind[1]]
+kpR = kp1[ind[0]]
+cv2.circle(centerL, (int(kpL.pt[0]),int(kpL.pt[1])), 4, (255, 0, 0), 1)
+cv2.imshow('one keypoint', centerL)
+cv2.circle(centerR, (int(kpR.pt[0]),int(kpR.pt[1])), 4, (255, 0, 0), 1)
+cv2.imshow('other keypoint', centerR)
+
+# gray = cv2.cvtColor(centerL, cv2.COLOR_BGR2GRAY)
+# sift = cv2.xfeatures2d.SIFT_create()
+#     # draw key points detected
+# img2 = cv2.drawKeypoints(gray, kpL, gray, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# cv2.imshow("grayframe", img2)
+#
+# gray = cv2.cvtColor(centerL, cv2.COLOR_BGR2GRAY)
+# sift = cv2.xfeatures2d.SIFT_create()
+#     # draw key points detected
+# img2 = cv2.drawKeypoints(gray, kpR, gray, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# cv2.imshow("grayframe", img2)
+
+#(row_index, col_index) = distance.argmin(0), distance.argmin(1)
+# ri = distance[row_index, np.arange(len(row_index))]
+# ci = distance[col_index, np.arange(len(col_index))]
+# print(ri)
+print('ciao')
+# print(ci)
+#print(distance[row_index,col_index])
+# print(des1)
+# print('ciao')
+# print(des2)
+
+
