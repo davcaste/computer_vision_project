41701307970c7b50d013d0414225a8c60d7a2acb
davcaste <55695300+davcaste@users.noreply.github.com>
2020-03-17 11:07:38 +0100

    division in stripes after calculating the kpts
    division in stripes after calculating the kpts assigning at each stripes the kpts with the relative y

--- project.py
+++ project.py
@@ -9,13 +9,14 @@ capL = cv2.VideoCapture('robotL.avi')
 capR = cv2.VideoCapture('robotR.avi')
 focal_lenght = 567.2  # in pixel
 baseline = 92.226  # in mm
-dim = 100
+dim = 50
 h_stripe = 10
 if not capL.isOpened():
     print("Error opening video stream or file")
 
 if not capR.isOpened():
     print("Error opening video stream or file")
+sift = cv2.xfeatures2d.SIFT_create()
 
 while capL.isOpened() and capR.isOpened():
     retL, frameL = capL.read()
@@ -28,91 +29,126 @@ while capL.isOpened() and capR.isOpened():
         cv2.rectangle(frameR, (int(frameR.shape[1]/2)-dim, int(frameR.shape[0]/2)-dim), (int(frameR.shape[1]/2)+dim, int(frameR.shape[0]/2)+dim), 250, 1)
         centerL = frameL[int(frameL.shape[0]/2)-dim: int(frameL.shape[0]/2)+dim, int(frameL.shape[1]/2)-dim:int(frameL.shape[1]/2)+dim]
         centerR = frameR[int(frameR.shape[0] / 2) - dim: int(frameR.shape[0] / 2) + dim, int(frameR.shape[1] / 2) - dim:int(frameR.shape[1] / 2) + dim]
-        # center_tempL = frameL[int(frameL.shape[0] / 2) - dim: int(frameL.shape[0] / 2) + dim,
-        #           int(frameL.shape[1] / 2) - dim:int(frameL.shape[1] / 2) + dim]
-        center_tempL = copy.deepcopy(centerL)
-        # center_tempR = frameR[int(frameR.shape[0] / 2) - dim: int(frameR.shape[0] / 2) + dim,
-        #           int(frameR.shape[1] / 2) - dim:int(frameR.shape[1] / 2) + dim]
-        center_tempR = copy.deepcopy(centerR)
 
         grayL = cv2.cvtColor(centerL, cv2.COLOR_BGR2GRAY)
         intermediateL = cv2.equalizeHist(grayL)
-        finalL = cv2.medianBlur(intermediateL, 5)
+        finalL = cv2.medianBlur(intermediateL, 3)
 
         grayR = cv2.cvtColor(centerR, cv2.COLOR_BGR2GRAY)
         intermediateR = cv2.equalizeHist(grayR)
         finalR = cv2.medianBlur(intermediateR, 5)
 
+# #     STATE OF ART FOUND OF CV2
+        # stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+        # disparity = stereo.compute(finalL, finalR)
+        # d = disparity/255
+        # #disparity = np.uint8(disparity)
+        # cv2.imshow('title',disparity/255)
+        # cv2.imshow('t1',finalL)
+        # print('dentro')
+
+        # center_tempL = frameL[int(frameL.shape[0] / 2) - dim: int(frameL.shape[0] / 2) + dim,
+        #           int(frameL.shape[1] / 2) - dim:int(frameL.shape[1] / 2) + dim]
+        # center_tempL = copy.deepcopy(centerL)
+        # # center_tempR = frameR[int(frameR.shape[0] / 2) - dim: int(frameR.shape[0] / 2) + dim,
+        # #           int(frameR.shape[1] / 2) - dim:int(frameR.shape[1] / 2) + dim]
+        # center_tempR = copy.deepcopy(centerR)
+        #
+        # grayL = cv2.cvtColor(centerL, cv2.COLOR_BGR2GRAY)
+        # intermediateL = cv2.equalizeHist(grayL)
+        # finalL = cv2.medianBlur(intermediateL, 5)
+        #
+        # grayR = cv2.cvtColor(centerR, cv2.COLOR_BGR2GRAY)
+        # intermediateR = cv2.equalizeHist(grayR)
+        # finalR = cv2.medianBlur(intermediateR, 5)
+
+
+# #     DIVISION IN STRIPES BEFORE COMPUTING THE KEYPOINTS
+        # image_stripesL = np.zeros((int(finalL.shape[1]/h_stripe), h_stripe, finalL.shape[0]), dtype='uint8')  # NxHxL where N = n of stripes, H = height of stripes, L = lenght of window
+        # image_stripesR = np.zeros((int(finalR.shape[1] / h_stripe), h_stripe, finalR.shape[0]), dtype='uint8')
+        # kpL = np.zeros((int(finalL.shape[1]/h_stripe)), dtype='object')
+        # desL = np.zeros(int(finalL.shape[1]/h_stripe), dtype='object')
+        # imgL = np.zeros((int(finalL.shape[1]/h_stripe), h_stripe, finalL.shape[0]), dtype='uint8')
+        #
+        # kpR = np.zeros((int(finalR.shape[1]/h_stripe)), dtype='object')
+        # desR = np.zeros(int(finalR.shape[1]/h_stripe), dtype='object')
+        # imgR = np.zeros((int(finalR.shape[1]/h_stripe), h_stripe, finalL.shape[0]), dtype='uint8')
+        # distance = np.zeros((int(finalR.shape[1] / h_stripe), ), dtype='object')
+        # maxL = 0
+        # maxR = 0
+        #
+        # sift = cv2.xfeatures2d.SIFT_create()
+        #
+        # for i in range(int(finalL.shape[1]/h_stripe)):
+        #     image_stripesL[i] = finalL[i*h_stripe:(i+1)*h_stripe, :]
+        #     image_stripesR[i] = finalR[i * h_stripe:(i + 1) * h_stripe, :]
+        #     kpL[i], desL[i] = sift.detectAndCompute(image_stripesL[i], None)
+        #     kpR[i], desR[i] = sift.detectAndCompute(image_stripesR[i], None)
+        #     try:
+        #         distance[i] = np.zeros((desL[i].shape[0], desR[i].shape[0]))
+        #         center_tempL[i*h_stripe:(i+1)*h_stripe,:,:] = cv2.drawKeypoints(image_stripesL[i], kpL[i], center_tempL[i], flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        #         center_tempR[i*h_stripe:(i+1)*h_stripe,:,:] = cv2.drawKeypoints(image_stripesR[i], kpR[i], center_tempR[i], flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        #         distance[i] = np.array(np.sqrt(np.sum((desL[i][:, np.newaxis, :] - desR[i][np.newaxis, :, :]) ** 2, axis=-1)))
+        #     except(AttributeError):
+        #         distance[i] = np.array([[1000000]])
+        #     # if len(kpL[i]) > maxL:
+        #     #     maxL = len(kpL[i])
+        #     # if len(kpR[i]) > maxR:
+        #     #     maxR = len(kpR[i])
+        #
+        # totalimageL = np.vstack([center_tempL[i*h_stripe:(i+1)*h_stripe,:,:] for i in range(int(finalL.shape[1]/h_stripe))])
+        # totalimageR = np.vstack(
+        #     [center_tempR[i * h_stripe:(i + 1) * h_stripe, :, :] for i in range(int(finalR.shape[1] / h_stripe))])
+        # kptL = np.empty((sum([len([kpL[i] for i in range(int(finalL.shape[1]/h_stripe))])])), dtype='object')
+        # kptR = np.empty((sum([len([kpR[i] for i in range(int(finalR.shape[1] / h_stripe))])])), dtype='object')
+        # destL = []
+        # destR = []
+        # kptL = np.concatenate([kpL[i] for i in range(int(finalL.shape[1]/h_stripe))]).ravel()
+        # kptR = np.concatenate([kpR[i] for i in range(int(finalR.shape[1] / h_stripe))]).ravel()
+        #
+        # destL.append([desL[i] for i in range(int(finalL.shape[1]/h_stripe))])
+        # destR.append([desR[i] for i in range(int(finalR.shape[1] / h_stripe))])
+        # # for i in range(int(finalL.shape[1] / h_stripe)):
+        # #     distance[i,:,:] = np.array(np.sqrt(np.sum((desL[i][:, np.newaxis, :] - desR[i][np.newaxis, :, :]) ** 2, axis=-1)))
+        # cv2.imshow('image_stripesL', totalimageL)
+        # cv2.imshow('image_stripesR', totalimageR)
+        # # imgL = cv2.drawKeypoints(totalimageL, kptL[:], totalimageL, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        # # imgR = cv2.drawKeypoints(totalimageR, kptR[:], totalimageR, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        # # cv2.imshow("skL", imgL)
+        # # cv2.imshow("skR", imgR)
+
+
+
+
+
+# #     DETECT THE KPTS IN THE WHOLE IMAGES
+        kp2, des2 = sift.detectAndCompute(finalL, None)
+        kp1, des1 = sift.detectAndCompute(finalR, None)
+    # draw key points detected
+        img2 = cv2.drawKeypoints(finalL, kp2, finalL, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        cv2.imshow("grayframeL", img2)
+        img1 = cv2.drawKeypoints(finalR, kp1, finalR, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        cv2.imshow("grayframeR", img1)
         image_stripesL = np.zeros((int(finalL.shape[1]/h_stripe), h_stripe, finalL.shape[0]), dtype='uint8')  # NxHxL where N = n of stripes, H = height of stripes, L = lenght of window
         image_stripesR = np.zeros((int(finalR.shape[1] / h_stripe), h_stripe, finalR.shape[0]), dtype='uint8')
-        kpL = np.zeros((int(finalL.shape[1]/h_stripe)), dtype='object')
-        desL = np.zeros(int(finalL.shape[1]/h_stripe), dtype='object')
-        imgL = np.zeros((int(finalL.shape[1]/h_stripe), h_stripe, finalL.shape[0]), dtype='uint8')
-
-        kpR = np.zeros((int(finalR.shape[1]/h_stripe)), dtype='object')
-        desR = np.zeros(int(finalR.shape[1]/h_stripe), dtype='object')
-        imgR = np.zeros((int(finalR.shape[1]/h_stripe), h_stripe, finalL.shape[0]), dtype='uint8')
-        distance = np.zeros((int(finalR.shape[1] / h_stripe), ), dtype='object')
-        maxL = 0
-        maxR = 0
-
-        sift = cv2.xfeatures2d.SIFT_create()
-
-        for i in range(int(finalL.shape[1]/h_stripe)):
-            image_stripesL[i] = finalL[i*h_stripe:(i+1)*h_stripe, :]
-            image_stripesR[i] = finalR[i * h_stripe:(i + 1) * h_stripe, :]
-            kpL[i], desL[i] = sift.detectAndCompute(image_stripesL[i], None)
-            kpR[i], desR[i] = sift.detectAndCompute(image_stripesR[i], None)
-            try:
-                distance[i] = np.zeros((desL[i].shape[0], desR[i].shape[0]))
-                center_tempL[i*h_stripe:(i+1)*h_stripe,:,:] = cv2.drawKeypoints(image_stripesL[i], kpL[i], center_tempL[i], flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-                center_tempR[i*h_stripe:(i+1)*h_stripe,:,:] = cv2.drawKeypoints(image_stripesR[i], kpR[i], center_tempR[i], flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-                distance[i] = np.array(np.sqrt(np.sum((desL[i][:, np.newaxis, :] - desR[i][np.newaxis, :, :]) ** 2, axis=-1)))
-            except(AttributeError):
-                distance[i] = np.array([[1000000]])
-            # if len(kpL[i]) > maxL:
-            #     maxL = len(kpL[i])
-            # if len(kpR[i]) > maxR:
-            #     maxR = len(kpR[i])
-
-        totalimageL = np.vstack([center_tempL[i*h_stripe:(i+1)*h_stripe,:,:] for i in range(int(finalL.shape[1]/h_stripe))])
-        totalimageR = np.vstack(
-            [center_tempR[i * h_stripe:(i + 1) * h_stripe, :, :] for i in range(int(finalR.shape[1] / h_stripe))])
-        kptL = np.empty((sum([len([kpL[i] for i in range(int(finalL.shape[1]/h_stripe))])])), dtype='object')
-        kptR = np.empty((sum([len([kpR[i] for i in range(int(finalR.shape[1] / h_stripe))])])), dtype='object')
-        destL = []
-        destR = []
-        kptL = np.concatenate([kpL[i] for i in range(int(finalL.shape[1]/h_stripe))]).ravel()
-        kptR = np.concatenate([kpR[i] for i in range(int(finalR.shape[1] / h_stripe))]).ravel()
-
-        destL.append([desL[i] for i in range(int(finalL.shape[1]/h_stripe))])
-        destR.append([desR[i] for i in range(int(finalR.shape[1] / h_stripe))])
-        # for i in range(int(finalL.shape[1] / h_stripe)):
-        #     distance[i,:,:] = np.array(np.sqrt(np.sum((desL[i][:, np.newaxis, :] - desR[i][np.newaxis, :, :]) ** 2, axis=-1)))
-        cv2.imshow('image_stripesL', totalimageL)
-        cv2.imshow('image_stripesR', totalimageR)
-        # imgL = cv2.drawKeypoints(totalimageL, kptL[:], totalimageL, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        # imgR = cv2.drawKeypoints(totalimageR, kptR[:], totalimageR, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-        # cv2.imshow("skL", imgL)
-        # cv2.imshow("skR", imgR)
+        kp_stripesL = np.zeros((int(finalR.shape[1] / h_stripe)), dtype='object')
+        kp_stripesR = np.zeros((int(finalL.shape[1] / h_stripe)), dtype='object')
+        for i in range(int(finalL.shape[1] / h_stripe)):
+            if kp_stripesL[i] == 0:
+                kp_stripesL[i] = []
+            if kp_stripesR[i] == 0:
+                kp_stripesR[i] = []
+            for j in range(len(kp2)):
+                if i*h_stripe <= kp2[j].pt[1] < (i+1)*h_stripe:
+                    kp_stripesL[i].append(kp2[j])
+            for j in range(len(kp1)):
+                if i * h_stripe <= kp1[j].pt[1] < (i + 1) * h_stripe:
+                    kp_stripesR[i].append(kp1[j])
 
 
 
 
 
-
-    #
-    #     kp2, des2 = sift.detectAndCompute(finalL, None)
-    # # draw key points detected
-    #     img2 = cv2.drawKeypoints(finalL, kp2, finalL, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    #     cv2.imshow("grayframe", img2)
-    #
-    #
-    #
-    #     sift = cv2.xfeatures2d.SIFT_create()
-    #
-    #     kp1, des1 = sift.detectAndCompute(finalR, None)
-    #
     #     # # BFMatcher with default params
     #     # bf = cv2.BFMatcher(cv2.NORM_L2)
     #     # matches = bf.knnMatch(des1, des2, k=2)
@@ -178,7 +214,7 @@ while capL.isOpened() and capR.isOpened():
     #
     #     cv2.imshow('FrameL', frameL)
     #     cv2.imshow('FrameR', frameR)
-    # Press Q on keyboard to  exit
+    # # Press Q on keyboard to  exit
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 
@@ -187,34 +223,34 @@ capR.release()
 
 cv2.destroyAllWindows()
 # print(distance)
-print('ciao')
-print(distance.min())
-ind = np.unravel_index(np.argmin(distance, axis=None), distance.shape)
-# print(ind)
-# print(distance[ind])
-# kpL = kp2[ind[1]]
-# kpR = kp1[ind[0]]
-# cv2.circle(centerL, (int(kpL.pt[0]),int(kpL.pt[1])), 4, (255, 0, 0), 1)
-# cv2.imshow('one keypoint', centerL)
-# cv2.circle(centerR, (int(kpR.pt[0]),int(kpR.pt[1])), 4, (255, 0, 0), 1)
-# cv2.imshow('other keypoint', centerR)
-
-# gray = cv2.cvtColor(centerL, cv2.COLOR_BGR2GRAY)
-# sift = cv2.xfeatures2d.SIFT_create()
-#     # draw key points detected
-# img2 = cv2.drawKeypoints(gray, kpL, gray, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-# cv2.imshow("grayframe", img2)
+# print('ciao')
+# print(distance.min())
+# ind = np.unravel_index(np.argmin(distance, axis=None), distance.shape)
+# # print(ind)
+# # print(distance[ind])
+# # kpL = kp2[ind[1]]
+# # kpR = kp1[ind[0]]
+# # cv2.circle(centerL, (int(kpL.pt[0]),int(kpL.pt[1])), 4, (255, 0, 0), 1)
+# # cv2.imshow('one keypoint', centerL)
+# # cv2.circle(centerR, (int(kpR.pt[0]),int(kpR.pt[1])), 4, (255, 0, 0), 1)
+# # cv2.imshow('other keypoint', centerR)
 #
-# gray = cv2.cvtColor(centerL, cv2.COLOR_BGR2GRAY)
-# sift = cv2.xfeatures2d.SIFT_create()
-#     # draw key points detected
-# img2 = cv2.drawKeypoints(gray, kpR, gray, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-# cv2.imshow("grayframe", img2)
-
-#(row_index, col_index) = distance.argmin(0), distance.argmin(1)
-# ri = distance[row_index, np.arange(len(row_index))]
-# ci = distance[col_index, np.arange(len(col_index))]
-# print(ri)
+# # gray = cv2.cvtColor(centerL, cv2.COLOR_BGR2GRAY)
+# # sift = cv2.xfeatures2d.SIFT_create()
+# #     # draw key points detected
+# # img2 = cv2.drawKeypoints(gray, kpL, gray, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# # cv2.imshow("grayframe", img2)
+# #
+# # gray = cv2.cvtColor(centerL, cv2.COLOR_BGR2GRAY)
+# # sift = cv2.xfeatures2d.SIFT_create()
+# #     # draw key points detected
+# # img2 = cv2.drawKeypoints(gray, kpR, gray, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+# # cv2.imshow("grayframe", img2)
+#
+# #(row_index, col_index) = distance.argmin(0), distance.argmin(1)
+# # ri = distance[row_index, np.arange(len(row_index))]
+# # ci = distance[col_index, np.arange(len(col_index))]
+# # print(ri)
 print('ciao')
 # print(ci)
 #print(distance[row_index,col_index])
